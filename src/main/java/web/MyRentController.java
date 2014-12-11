c32ffbc864393f0bae8f6402b0ad9b55ac2dac6d
unknown <ben@pc-charroux.(none)>
2014-12-11 12:37:43 +0100

    get + post + put

--- src/main/java/web/MyRentController.java
+++ src/main/java/web/MyRentController.java
@@ -20,30 +20,67 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class MyRentController{
 
 
+	List<Car> cars;
+	
+	
+	public MyRentController() {
+		super();
+		cars = new ArrayList<Car>();
+		
+		Car car = new Car();
+		car.setNumberPlate("EEE 55 TT");
+		car.setRented(false);
+		cars.add(car);
+		
+		car = new Car();
+		car.setNumberPlate("AA 11 GG");
+		car.setRented(false);
+		cars.add(car);
+	}
+
 	@RequestMapping(value = "/car", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public List<Car> getCars() {
-		return null; 
+		return cars; 
 	}
 
 	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public Car getCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		return null;
+		int i=0;
+		while(i<cars.size() && cars.get(i).getNumberPlate().equals(plateNumber)==true){
+			i++;
+		}
+		if(i < cars.size()){
+			return cars.get(i);
+		}
+		throw new Exception("No car: " + plateNumber);
 	}
 
 	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.DELETE)
 	@ResponseStatus(HttpStatus.OK)
 	public void rentCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		
+		int i=0;
+		while(i<cars.size() && cars.get(i).getNumberPlate().equals(plateNumber)==true){
+			i++;
+		}
+		if(i < cars.size()){
+			cars.get(i).setRented(true);
+		}
 	}
 
 	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.PUT)
 	@ResponseStatus(HttpStatus.OK)
 	public void renderCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		
+		int i=0;
+		while(i<cars.size() && cars.get(i).getNumberPlate().equals(plateNumber)==true){
+			i++;
+		}
+		if(i < cars.size()){
+			cars.get(i).setRented(false);
+		}
 	}
 
 	
