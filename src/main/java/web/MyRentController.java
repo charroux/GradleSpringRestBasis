f35f7e3b07690c92513853764aedc3799e1576d9
charroux <benoit.charroux@free.fr>
2015-02-23 13:11:56 +0100

    codage de la methode get car

--- src/main/java/web/MyRentController.java
+++ src/main/java/web/MyRentController.java
@@ -1,10 +1,18 @@
 package web;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import model.Car;
+
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import dto.CarDTO;
 
@@ -12,6 +20,23 @@ import dto.CarDTO;
 @Component
 public class MyRentController implements RentService{
 
+	List<Car> cars = new ArrayList<Car>();
+	
+	public MyRentController(){
+		
+		Car car1 = new Car();
+		car1.setPlateNumber("AA 11 AA");
+		car1.setRented(true);
+		
+		cars.add(car1);
+		
+		Car car2 = new Car();
+		car2.setPlateNumber("BB 22 BB");
+		car2.setRented(false);
+		
+		cars.add(car2);
+	}
+	
 	@Override
 	public ResourceSupport get() {
 		// TODO Auto-generated method stub
@@ -19,9 +44,21 @@ public class MyRentController implements RentService{
 	}
 
 	@Override
+	@RequestMapping(value = "/car", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
 	public List<CarDTO> getCars() {
-		// TODO Auto-generated method stub
-		return null;
+		List<CarDTO> dtos = new ArrayList<CarDTO>();
+		Car car;
+		for(int i=0; i<cars.size(); i++){
+			car = cars.get(i);
+			if(car.isRented() == false){
+				CarDTO dto = new CarDTO();
+				dto.setPlateNumber(car.getPlateNumber());
+				dtos.add(dto);
+			}
+		}
+		return dtos;
 	}
 
 	@Override
