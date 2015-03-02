718ec5bc4d502f7197b91e0f6e82bf4453d7426b
charroux <benoit.charroux@free.fr>
2015-03-02 16:58:49 +0100

    codage de la methode get pour une voiture

--- src/main/java/web/MyRentController.java
+++ src/main/java/web/MyRentController.java
@@ -9,6 +9,7 @@ import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,11 +38,18 @@ public class MyRentController implements RentService{
 	}
 	
 	@Override
+	@RequestMapping(value = "/entryPoint", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
 	public ResourceSupport get() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	*
+	* @return all cars not rented
+	*/
 	@Override
 	@RequestMapping(value = "/car", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
@@ -59,18 +67,52 @@ public class MyRentController implements RentService{
 		return dtos;		
 	}
 
+	/**
+	* Return specifications of a car.
+	* @param plateNumber
+	* @return car specifications only (if not rented)
+	* @throws Exception no car with this plate number or already rented
+	*/
+	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
 	@Override
-	public CarDTO getCar(String plateNumber) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public CarDTO getCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
+		System.out.println(plateNumber);
+		int i=0;
+		while(i<cars.size() && cars.get(i).getPlateNumber().equals(plateNumber)==false){
+			i++;
+		}
+		
+		if(i<cars.size()){
+			CarDTO dto = new CarDTO(cars.get(i));
+			return dto;
+		}
+		
+		throw new Exception("No car with such a plate number");
 	}
 
+	/**
+	* Rent a car.
+	* @param plateNumber
+	* @return car specifications
+	* @throws Exception no car with this plate number or already rented
+	*/
+	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
 	@Override
 	public void rentCar(String plateNumber) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	*
+	** @return actions to be done
+	* @throws Exception no car with this plate number or not rented
+	*/
+	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
 	@Override
 	public void renderCar(String plateNumber) throws Exception {
 		// TODO Auto-generated method stub
