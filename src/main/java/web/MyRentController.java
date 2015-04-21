74b7019159851783aa35ae5fddf2819f3fea53c7
charroux <benoit.charroux@free.fr>
2015-04-21 16:22:57 +0200

    codage algorithmes

--- src/main/java/web/MyRentController.java
+++ src/main/java/web/MyRentController.java
@@ -38,7 +38,7 @@ public class MyRentController implements RentService{
 	@ResponseBody
 	@Override
 	public ResourceSupport get() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -51,21 +51,37 @@ public class MyRentController implements RentService{
 	@ResponseBody
 	@Override
 	public List<CarDTO> getCars() {
-		return null;
+		CarDTO carDTO;
+		List<CarDTO> dtos = new ArrayList<CarDTO>();
+		for(Car car : cars){
+			if(car.isRented() == false){
+				carDTO = new CarDTO(car);
+				dtos.add(carDTO);
+			}
+		}
+		return dtos;
 	}
 
 	/**
 	* Return specifications of a car.
 	* @param plateNumber
 	* @return car specifications only (if not rented)
-	* @throws Exception no car with this plate number or already rented
+	* @throws Exception no car with this plate number
 	*/
 	@RequestMapping(value = "/car/{plateNumber}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	@Override
 	public CarDTO getCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		return null;
+		int  i = 0;
+		while(i<cars.size() && cars.get(i).getPlateNumber().equals(plateNumber)==false){
+			i++;
+		}
+		if(i < cars.size()){
+			return new CarDTO(cars.get(i));
+		} else {
+			throw new Exception("No car with such a plate number");
+		}
 	}
 
 	/**
@@ -78,7 +94,19 @@ public class MyRentController implements RentService{
 	@ResponseStatus(HttpStatus.OK)
 	@Override
 	public void rentCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		
+		int  i = 0;
+		while(i<cars.size() && cars.get(i).getPlateNumber().equals(plateNumber)==false){
+			i++;
+		}
+		if(i < cars.size()){
+			if(cars.get(i).isRented() == false){
+				cars.get(i).setRented(true);
+			} else {
+				throw new Exception("Car already rented");
+			}
+		} else {
+			throw new Exception("No car with such a plate number");
+		}
 	}
 
 	/**
@@ -90,7 +118,17 @@ public class MyRentController implements RentService{
 	@ResponseStatus(HttpStatus.OK)
 	@Override
 	public void renderCar(@PathVariable("plateNumber") String plateNumber) throws Exception {
-		
+		int  i = 0;
+		while(i<cars.size() && cars.get(i).getPlateNumber().equals(plateNumber)==false){
+			i++;
+		}
+		if(i < cars.size()){
+			if(cars.get(i).isRented() == true){
+				cars.get(i).setRented(false);
+			} 
+		} else {
+			throw new Exception("No car with such a plate number");
+		}
 	}
 
 }
